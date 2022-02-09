6c1baec37e7a3e1884217b1084c51ce43cfc84f8
Sebastian Reimers <hallo@studio-link.de>
2022-02-09 14:03:09 +0100

    fix tmr integration tests

--- src/tmr.c
+++ src/tmr.c
@@ -18,9 +18,6 @@ int test_tmr_jiffies(void)
 	uint64_t tmr_start, tmr_end, diff;
 	int err = 0;
 
-	if (test_mode != TEST_REGULAR)
-		return ESKIPPED;
-
 	tmr_start = tmr_jiffies();
 	sys_msleep(1);
 	tmr_end = tmr_jiffies();
@@ -39,9 +36,6 @@ int test_tmr_jiffies_usec(void)
 	uint64_t tmr_start, diff;
 	int i;
 	int err = 0;
-
-	if (test_mode != TEST_REGULAR)
-		return ESKIPPED;
 
 	tmr_start = tmr_jiffies_usec();
 	diff = 0;
