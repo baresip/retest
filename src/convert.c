d4140b9b2204be2e41d61ed221fc29d8209b5239
Sebastian Reimers <hallo@studio-link.de>
2022-03-23 12:02:03 +0100

    format

--- src/convert.c
+++ src/convert.c
@@ -13,44 +13,44 @@
 
 int test_try_into(void)
 {
-	int err	 = 0;
+	int err = 0;
 	size_t size;
 	uint16_t u16 = 0;
 	int i;
 
 	/* Testing size_t -> uint16_t */
 	size = SIZE_MAX;
-	err = try_into(u16, size);
+	err  = try_into(u16, size);
 	TEST_EQUALS(ERANGE, err);
 
 	size = 5000;
-	err = try_into(u16, size);
+	err  = try_into(u16, size);
 	TEST_ERR(err);
 	TEST_EQUALS(size, u16);
 
 	size = SIZE_MAX;
-	err = try_into(u16, size);
+	err  = try_into(u16, size);
 	TEST_EQUALS(ERANGE, err);
 
 	/* Testing int -> uint16_t */
-	i = INT_MAX;
+	i   = INT_MAX;
 	err = try_into(u16, i);
 	TEST_EQUALS(ERANGE, err);
-	
-	i = -50;
+
+	i   = -50;
 	err = try_into(u16, i);
 	TEST_EQUALS(ERANGE, err);
 
 	/* Testing size_t -> int */
 	size = SIZE_MAX;
-	err = try_into(i, size);
+	err  = try_into(i, size);
 	TEST_EQUALS(ERANGE, err);
 
 	size = INT_MAX;
-	err = try_into(i, size);
+	err  = try_into(i, size);
 	TEST_ERR(err);
 	TEST_EQUALS(INT_MAX, i);
-	
+
 out:
 	return err;
 }
