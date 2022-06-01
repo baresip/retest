42808996b871c2ba094b2781e57a6559ff168d43
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2022-06-01 09:16:10 +0200

    av1: add testing of obu decode (#72)

--- src/av1.c
+++ src/av1.c
@@ -15,7 +15,7 @@
 #include <re_dbg.h>
 
 
-int test_av1(void)
+static int test_av1_aggr(void)
 {
 	static const struct test {
 		uint8_t byte;
@@ -53,5 +53,66 @@ int test_av1(void)
 	}
 
  out:
+	return err;
+}
+
+
+static int test_av1_obu(void)
+{
+	struct av1_obu_hdr hdr;
+	static const uint8_t buf[] = {
+
+		/* libaom OBU_TEMPORAL_DELIMITER [type=2 x=0 s=1 size=0] */
+		0x12, 0x00,
+
+		/* libaom OBU_SEQUENCE_HEADER [type=1 x=0 s=1 size=12] */
+		0x0a, 0x0c, 0x00, 0x00,
+		0x00, 0x04, 0x3c, 0xff,
+		0xbf, 0x81, 0xb5, 0x32,
+		0x00, 0x80
+	};
+	struct mbuf mb = {
+		.buf  = (uint8_t *)buf,
+		.size = sizeof(buf),
+		.pos  = 0,
+		.end  = sizeof(buf)
+	};
+	int err;
+
+	err = av1_obu_decode(&hdr, &mb);
+	if (err)
+		goto out;
+
+	ASSERT_EQ(2, hdr.type);
+	ASSERT_EQ(0, hdr.x);
+	ASSERT_EQ(1, hdr.s);
+	ASSERT_EQ(0, hdr.size);
+
+	err = av1_obu_decode(&hdr, &mb);
+	if (err)
+		goto out;
+
+	ASSERT_EQ(1, hdr.type);
+	ASSERT_EQ(0, hdr.x);
+	ASSERT_EQ(1, hdr.s);
+	ASSERT_EQ(12, hdr.size);
+
+ out:
+	return err;
+}
+
+
+int test_av1(void)
+{
+	int err;
+
+	err = test_av1_aggr();
+	if (err)
+		return err;
+
+	err = test_av1_obu();
+	if (err)
+		return err;
+
 	return err;
 }
