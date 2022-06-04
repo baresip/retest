6dd12b53e12927db9cf9395808e1c35d9bceb8e3
Sebastian Reimers <hallo@studio-link.de>
2022-06-04 13:31:55 +0200

    cmake: add relative path hints for libre and librem

--- cmake/Findrem.cmake
+++ cmake/Findrem.cmake
@@ -1,9 +1,9 @@
 find_package(PkgConfig QUIET)
 pkg_check_modules(PC_LIBREM QUIET librem)
 
-find_path(REM_INCLUDE_DIR rem.h HINTS ${PC_LIBREM_INCLUDEDIR} ${PC_LIBREM_INCLUDE_DIRS})
+find_path(REM_INCLUDE_DIR rem.h HINTS ../rem/include ${PC_LIBREM_INCLUDEDIR} ${PC_LIBREM_INCLUDE_DIRS})
 
-find_library(REM_LIBRARY NAMES rem librem HINTS ${PC_LIBREM_LIBDIR} ${PC_LIBREM_LIBRARY_DIRS})
+find_library(REM_LIBRARY NAMES rem librem HINTS ../rem ${PC_LIBREM_LIBDIR} ${PC_LIBREM_LIBRARY_DIRS})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(rem DEFAULT_MSG REM_LIBRARY REM_INCLUDE_DIR)
